9be5b736c7a1085e129ac13fab42c925225d06d7
regyap <regineyap1609@gmail.com>
2024-04-16 01:09:18 +0800

    help

--- lifelong-learning/src/Components/JobCardNoSkill/JobCardNoSkill.jsx
+++ lifelong-learning/src/Components/JobCardNoSkill/JobCardNoSkill.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import ProgressBar from '@ramonak/react-progress-bar';
 // import JobCard from '../JobCardNoSkill/JobCardNoSkill.jsx';
-import "./jobcardnoskill.css";
+import './jobcardnoskill.css';
 // help
 
 const JobCardNoSkill = ({ job, handleJobClick, onApply }) => {
